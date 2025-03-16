bfb58cd403671d666c2f27096203567b3e504044
Eugene Gan Cheong Wei <eugene_gan123@hotmail.com>
2025-03-16 14:27:10 +0800

    fix: updated script to allow for new library and chunks to be stored in chroma vector database.

--- create_database.py
+++ create_database.py
@@ -1,21 +1,55 @@
-# from langchain.document_loaders import DirectoryLoader
-from langchain_community.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema import Document
-# from langchain.embeddings import OpenAIEmbeddings
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-import openai 
-from dotenv import load_dotenv
+import logging
 import os
 import shutil
 
+from dotenv import load_dotenv
+import nltk
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma.vectorstores import Chroma
+from langchain_openai import AzureChatOpenAI
+
+# from langchain.embeddings import OpenAIEmbeddings
+# from langchain_openai import AzureOpenAIEmbeddings
+from langchain_community.document_loaders import DirectoryLoader
+
+nltk.download("punkt", download_dir="~/nltk_data")
+nltk.download("averaged_perceptron_tagger", download_dir="~/nltk_data")
+
+
+# Configure logger
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 # Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
-#---- Set OpenAI API key 
-# Change environment variable name from "OPENAI_API_KEY" to the name given in 
-# your .env file.
-openai.api_key = os.environ['OPENAI_API_KEY']
+
+# ---- Set Azure OpenAI API key and endpoint
+azure_openai_api_key = os.environ["AZURE_OPENAI_API_KEY"]
+azure_openai_endpoint = os.environ["AZURE_OPENAI_ENDPOINT"]
+api_version = os.environ["API_VERSION"]
+deployment_name = os.environ["DEPLOYMENT_NAME"]
+# embedding_model = os.environ["EMBEDDING_MODEL"]
+
+# Initialize Azure OpenAI client
+client = AzureChatOpenAI(
+    azure_endpoint=azure_openai_endpoint,
+    api_key=azure_openai_api_key,
+    api_version=api_version,
+    deployment_name=deployment_name,
+    temperature=0.2,
+)
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# embeddings = AzureOpenAIEmbeddings(
+#     azure_endpoint=azure_openai_endpoint,
+#     azure_deployment=embedding_model,  # Ensure this is an embedding model like 'text-embedding-ada-002'
+#     openai_api_version=api_version,
+#     api_key=azure_openai_api_key,
+# )
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data/books"
@@ -59,11 +93,15 @@ def save_to_chroma(chunks: list[Document]):
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
-    # Create a new DB from the documents.
-    db = Chroma.from_documents(
-        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    # Initialize the Chroma vector store
+    vector_store = Chroma(
+        collection_name="my_collection",
+        embedding_function=embeddings,
+        persist_directory=CHROMA_PATH,
     )
-    db.persist()
+
+    # Add documents to the vector store
+    vector_store.add_documents(documents=chunks)
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
 
 
